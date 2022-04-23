5efb2d47160939b8159ce6753abe4264b9ad2e77
Pinglei He <pingleihe@outlook.com>
2022-04-23 21:14:03 +0200

    make the psnr implementation more pythonic

--- evaluation/PSNR.py
+++ evaluation/PSNR.py
@@ -1,10 +1,19 @@
+"""
+Calculation of the Peak Signal-to-Noise Ratio (PSNR).
+Author: Pinglei He
+Heavily based on the Python implementation of PSNR by Huang Liu
+"""
+
 import math
 import numpy
 
 
 def PSNR(img1, img2):
-    D = numpy.array(img1 - img2, dtype=numpy.int64)
-    D[:, :] = D[:, :] ** 2
-    RMSE = D.sum() / img1.size
-    psnr = 10 * math.log10(float(255. ** 2) / RMSE)
+    """Calculate peak signal-to-noise ratio (PSNR) between two images."""
+    arr_1 = numpy.array(img1, dtype=numpy.int64)
+    arr_2 = numpy.array(img2, dtype=numpy.int64)
+    diff = arr_1 - arr_2  # difference between the two images
+    diff_squared = diff[:, :] ** 2  # square the difference
+    rmse = diff_squared.sum() / img1.size  # calculate the root mean squared error
+    psnr = 10 * math.log10(float(255. ** 2) / rmse)  # calculate the PSNR
     return psnr
