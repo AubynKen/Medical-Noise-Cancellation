3dc99cbdc87d42a870bdfae73720faaf156ce260
Pinglei He <pingleihe@outlook.com>
2022-04-26 01:04:11 +0200

    added contrast-adjusted images for comparison purposes

---
+++ archives/archived code/contrast_adjustment.py
@@ -0,0 +1,19 @@
+"""
+The noisy images and the base images do not have the same color contrast because the normalization step during the
+conversion from raw format to png format is done after image normalization. Here we create base images with the same
+contrast as the noisy images, so that the base images and noisy images have the same mean.
+"""
+
+import numpy as np
+from PIL import Image
+import os
+
+base_path = "../../data/dataset/base_png"
+files = os.listdir("../../data/dataset/base_png")
+
+for file in files:
+    im = Image.open(os.path.join(base_path, file))
+    im = np.array(im, dtype="float")
+    im = im / 3 + 15802.056997617085
+    im = Image.fromarray(im.astype(np.uint16))
+    im.save(f"data/dataset/base_png_adjusted/{file}")
