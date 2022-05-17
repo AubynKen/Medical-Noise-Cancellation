5e8d28eafbbf23059af7b6d09c7cf40fd48e41df
Pinglei He <pingleihe@outlook.com>
2022-05-17 18:46:58 +0200

    adjustment of validation set for validation

---
+++ data/code/normalization_to_16bits.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+import numpy as np
+
+path = "../dataset/validation_set_clean"
+for file in os.listdir(path):
+    if not file.endswith(".png"):
+        continue
+    im = Image.open(os.path.join(path, file))
+    arr = np.array(im, dtype="float")
+    arr = (arr * (2 ** 16 - 1) / np.max(arr)).astype("uint16")
+    im = Image.fromarray(arr)
+    print(im.mode)
+    im.save(os.path.join("../dataset/validation_set_bis", file))
+    im = Image.open(os.path.join("../dataset/validation_set_bis", file))
+    print(im.mode)
